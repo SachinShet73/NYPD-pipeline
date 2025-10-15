504c0eb40d6deeeb44f6682757acc3d367b9ce1b
Sachin Shet <sachinshet135@gmail.com>
2025-10-15 17:09:30 -0400

    trimmed data for altreyx

--- profiler.py
+++ profiler.py
@@ -2,7 +2,7 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 # Load the data
-df = pd.read_csv('data/raw/NYPD_Arrests_Data__Historic_.csv')
+df = pd.read_csv('data/raw/NYPD_Arrests_Data__Historic_reduced.csv')
 
 # Generate profiling report
 profile = ProfileReport(df, 
